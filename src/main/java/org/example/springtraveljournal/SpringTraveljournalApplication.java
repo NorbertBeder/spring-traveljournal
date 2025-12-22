46906c5b4a7f69fa86d4733e6d023ea4427d6ce3
nbeder <norbert.beder@endava.com>
2025-12-22 10:52:55 +0200

    init commit

---
+++ src/main/java/org/example/springtraveljournal/SpringTraveljournalApplication.java
@@ -0,0 +1,13 @@
+package org.example.springtraveljournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTraveljournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTraveljournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springtraveljournal/SpringTraveljournalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springtraveljournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTraveljournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
